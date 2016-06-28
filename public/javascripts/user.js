29b0b797037fbde5b5fa5a29bf0cb9ff576c328a
jzhangkun <jzhangkun1986@gmail.com>
2016-06-28 22:02:33 +0800

    user page

--- public/javascripts/user.js
+++ public/javascripts/user.js
@@ -1,52 +1,44 @@
-var jQuery = require("jQuery");
 (function( $ ) {
  
   // Pre-compile templates and "cache" them using closure
-  var
-    userTemplate = _.template($( "#userTemplate" ).html()),
-    ratingsTemplate = _.template($( "#ratingsTemplate" ).html());
+  //var
+  //  userTemplate = _.template($( "#userTemplate" ).html()),
+  //  ratingsTemplate = _.template($( "#ratingsTemplate" ).html());
   
-   console.log(userTemplate);
+  var userTemplate = jade.compile('li name = #{name}');
+  var ratingsTemplate = jade.compile('li #{title} was rated #{rating}');
+
+  //console.log(userTemplate);
 
   // Subscribe to the new user topic, which adds a user
   // to a list of users who have submitted reviews
   $.subscribe( "/new/user", function( e, data ){
- 
     if( data ){
- 
       $('#users').append( userTemplate( data ));
- 
     }
- 
   });
  
   // Subscribe to the new rating topic. This is composed of a title and
   // rating. New ratings are appended to a running list of added user
   // ratings.
   $.subscribe( "/new/rating", function( e, data ){
- 
     if( data ){
- 
       $( "#ratings" ).append( ratingsTemplate( data ) );
- 
     }
- 
   });
  
   // Handler for adding a new user
   $("#add").on("click", function( e ) {
- 
     e.preventDefault();
- 
     var strUser = $("#twitter_handle").val(),
-       strMovie = $("#movie_seen").val(),
-       strRating = $("#movie_rating").val();
- 
+        strMovie = $("#movie_seen").val(),
+        strRating = $("#movie_rating").val();
+
     // Inform the application a new user is available
     $.publish( "/new/user", { name: strUser } );
  
     // Inform the app a new rating is available
     $.publish( "/new/rating", { title: strMovie, rating: strRating} );
  
-  }
+  });
 })( jQuery );
--- t/userCompile.js
+++ t/userCompile.js
@@ -5,5 +5,3 @@ var userJade = jade.compileFile('./views/user.jade', {pretty: true});
 var html = userJade({title:"test"});
 
 console.log(html);
-
-var scriptJade = jade.compileFile('./views/script.jade', {pretty: true});
